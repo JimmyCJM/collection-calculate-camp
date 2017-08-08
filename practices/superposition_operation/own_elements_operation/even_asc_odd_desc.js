b4b903ccf65662968fd579a4c852c1b8ba8933bb
JimmyCJM <chijieming@sina.com>
2017-08-08 17:02:45 +0800

    Update even_asc_odd_desc.js

--- practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,27 @@
 'use strict';
 var even_asc_odd_desc = function(collection){
-
+  let collection_a = collection.filter(value => {
+    if(value%2 === 0) return 1;
+    else return 0;
+  });
+  let collection_b = collection.filter(value => {
+    if(value%2 === 0) return 0;
+    else return 1;
+  })
+  collection_a = collection_a.sort(asc);
+  collection_b = collection_b.sort(dec);
+  collection_a = collection_a.concat(collection_b);
+  return  collection_a;
 };
+
+function asc(first,second) {
+  if(first === second) return 0;
+  if(first > second)  return 1;
+  if(first < second)  return -1;
+}
+function dec(first,second) {
+  if(first === second) return 0;
+  if(first > second)  return -1;
+  if(first < second)  return 1;
+}
 module.exports = even_asc_odd_desc;
