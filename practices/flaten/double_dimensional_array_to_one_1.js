b72437d7466a838eb55e2d51138f39846f7e22b5
JimmyCJM <chijieming@sina.com>
2017-08-08 10:03:47 +0800

    Update double_dimensional_array_to_one_1.js

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+  var result = new Array();
+  collection.forEach(value => result = result.concat(value));
+  return result;
 }
 
 module.exports = double_to_one;
