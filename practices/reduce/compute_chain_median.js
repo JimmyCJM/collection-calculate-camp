663022d43a99953f1b41a86781bbcadb4c13f079
JimmyCJM <chijieming@sina.com>
2017-08-08 10:21:38 +0800

    Update compute_chain_median.js

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  //在这里写入代码
+  collection = ((chain.split("->")).map(value => parseInt(value))).sort(compare);
+  if (collection.length%2 === 0) return (collection[collection.length/2 - 1]+collection[collection.length/2])/2;
+  else return collection[(collection.length-1)/2];
+}
+
+function compare(first,second) {
+  if(first === second) return 0;
+  if(first>second) return 1;
+  if(first<second) return -1;
 }
 
 module.exports = compute_chain_median;
