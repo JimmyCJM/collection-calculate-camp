cdc7a6e686f6f651ea3eed7b579a83e48cdfd5f7
JimmyCJM <chijieming@sina.com>
2017-08-08 08:48:57 +0800

    Update collect_all_even.js

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
+  var result = new Array();
+  collection.forEach(value => {
+    if ((value % 2) !== 1){
+      result.push(value);
+    }
+  })
+  return result;
 }
 
 module.exports = collect_all_even;
