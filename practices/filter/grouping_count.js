4dbea4499d94fd80a839ad6ed0d05882fb44e535
JimmyCJM <chijieming@sina.com>
2017-08-08 09:24:59 +0800

    Update grouping_count.js

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,21 @@
 'use strict';
 
 function grouping_count(collection) {
-
-  //在这里写入代码
+  var result = collection.reduce(function (p,k){
+    if (k in p){
+      p[k]++
+    }
+    else {
+      p[k] = 1
+    }
+    return p;
+  },{});
+  return result;
 }
+/*
+function grouping_count(collection) {
+  return collection.reduce((p,k) => (p[k]++ || (p[k] = 1),p),{});
+}
+*/
 
 module.exports = grouping_count;
