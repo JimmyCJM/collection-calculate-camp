5438d5f5fcade6daa2b9224aa64916c64b3c4a4e
JimmyCJM <chijieming@sina.com>
2017-08-08 09:12:25 +0800

    Update choose_multiples_of_three.js

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-
-  //在这里写入代码
+  return  collection.filter(value =>{
+    if(value%3 === 0) return 1;
+    else return 0;
+  })
 }
 
 module.exports = choose_multiples_of_three;
