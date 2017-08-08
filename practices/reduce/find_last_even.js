716b23d978b8931018517eb0cb1739aa29919db2
JimmyCJM <chijieming@sina.com>
2017-08-08 10:25:17 +0800

    Update find_last_even.js

--- practices/reduce/find_last_even.js
+++ practices/reduce/find_last_even.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function find_last_even(collection) {
-  //在这里写入代码
+  var result = collection.filter(value => {
+    if (value%2 === 0) return 1;
+    else return 0;
+  });
+  return result[result.length - 1];
 }
 
 module.exports = find_last_even;
