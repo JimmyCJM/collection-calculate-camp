6c01431609f5152150943ff42faae4e28b86e064
JimmyCJM <chijieming@sina.com>
2017-08-08 17:02:07 +0800

    Update single_element.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,17 @@
 'use strict';
 var single_element = function(collection){
-
+  var result = [];
+  var del = [];
+  collection.filter((value,index) => {
+    if (index%2 === 1) return 1;
+    else return 0;
+  }).forEach(value => {
+    if (result.indexOf(value) === -1) result.push(value);
+    else del.push(result.indexOf(value));
+  })
+  del.forEach(value => {
+    result.splice(value,1);
+  })
+  return result;
 };
 module.exports = single_element;
