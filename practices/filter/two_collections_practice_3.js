eb843cd6c70c0c2f5f6029de7434dfae6787e771
JimmyCJM <chijieming@sina.com>
2017-08-08 09:58:21 +0800

    Update two_collections_practice_3.js

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -1,8 +1,13 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-
-  //在这里写入代码
+  return collection_a.filter(value =>{
+    for (let i of collection_b){
+      if(value%i === 0){
+        return value;
+      }
+    }
+  });
 }
 
 module.exports = choose_divisible_integer;
