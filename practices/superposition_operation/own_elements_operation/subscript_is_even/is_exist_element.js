994c5778f368b59775c5458beeb1bb8808e80a7f
JimmyCJM <chijieming@sina.com>
2017-08-08 17:01:51 +0800

    Update is_exist_element.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,16 @@
 'use strict';
 var is_exist_element = function(collection,element){
-
+  var result = 0;
+  collection = collection.filter((value,index) =>{
+    if (index%2 === 0) return 1;
+    else return 0;
+  });
+  for (let i=0;i<collection.length;i++){
+    if (collection.indexOf(number) !== -1){
+      result = true;
+    }
+    else result = false;
+  }
+  return result;
 };
 module.exports = is_exist_element;
