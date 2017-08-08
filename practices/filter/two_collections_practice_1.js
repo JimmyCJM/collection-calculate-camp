2911e7034e9bdf80041e739231dbf8cac3f7ca5c
JimmyCJM <chijieming@sina.com>
2017-08-08 09:57:17 +0800

    Update two_collections_practice_1.js

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -1,10 +1,7 @@
 'use strict';
 
 function choose_common_elements(collection_a, collection_b) {
-  return collection_a.filter(value => {
-    if(collection_b.includes(value)) return 1;
-    else return 0;
-  });
+  return collection_a.filter(function (value) {return collection_b.indexOf(value) !== -1;});
 }
 
 module.exports = choose_common_elements;
