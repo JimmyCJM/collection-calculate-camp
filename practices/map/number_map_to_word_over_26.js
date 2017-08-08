a4a0d51f60f5d3c3514a2d05f1675ffa0da355d4
JimmyCJM <chijieming@sina.com>
2017-08-08 10:10:31 +0800

    Update number_map_to_word_over_26.js

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,9 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  return collection.map(value =>  {
+    if (value < 27) return String.fromCharCode(value+96);
+    else return String.fromCharCode(value/26+96) + String.fromCharCode(value%26+96);
+});
 };
 
 module.exports = number_map_to_word_over_26;
