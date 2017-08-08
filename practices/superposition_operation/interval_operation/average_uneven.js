fb3d3241585ac10e52430f7bd0245e8daa6ed07c
JimmyCJM <chijieming@sina.com>
2017-08-08 16:56:26 +0800

    Update average_uneven.js

--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function average_uneven(collection) {
-
-  //在这里写入代码
+  var a = collection.filter(value => {return value%2 === 1});
+  return a.reduce((p,value) => {
+    return p+= value},0)/a.length
 }
 
 module.exports = average_uneven;
