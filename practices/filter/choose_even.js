2a27992bafe575a6cbbd897579a78b812164603e
JimmyCJM <chijieming@sina.com>
2017-08-08 09:10:36 +0800

    Update choose_even.js

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function choose_even(collection) {
-
-  //在这里写入代码
+  return collection.filter(value =>{
+    if(value%2 === 0) return 1;
+    else return 0;
+  })
 }
 
 module.exports = choose_even;
