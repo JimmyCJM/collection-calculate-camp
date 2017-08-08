b98363e2fa6879b20066724b9071cd25e9abc8db
JimmyCJM <chijieming@sina.com>
2017-08-08 10:27:20 +0800

    Update practice.js

--- practices/section_1/practice_2/practice.js
+++ practices/section_1/practice_2/practice.js
@@ -1,5 +1,15 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+  var result = new Array();
+  for(var i=0;i<collection_a.length;i++){
+    for(var j=0;j<collection_b.length;j++){
+      for (var k=0;k<collection_b[j].length;k++){
+        if(collection_a[i] == collection_b[j][k]){
+          result.push(collection_a[i]);
+        }
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = collect_same_elements;
