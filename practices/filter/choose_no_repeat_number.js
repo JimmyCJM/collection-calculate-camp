f872a07fb5978c7b4b643ac5fed1afce1d7da87d
JimmyCJM <chijieming@sina.com>
2017-08-08 09:21:38 +0800

    Update choose_no_repeat_number.js

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
-
-  //在这里写入代码
+  var result = [];
+  collection.filter((value,index) =>{
+    if(result.includes(value)) return 0;
+    else {
+      result.push(value);
+    }return 1;
+  })
+  return result;
 }
 
 module.exports = choose_no_repeat_number;
