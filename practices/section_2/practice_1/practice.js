d3ac95dadc298794bbec2b742287d882027959ca
JimmyCJM <chijieming@sina.com>
2017-08-08 10:29:09 +0800

    Update practice.js

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,19 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  var result = new Array();
+  var n = 0,count = 1;
+  for (var i=0;i<collection.length;i++){
+
+    if(collection[i] == collection[i+1]){
+      count++;
+    }
+    else {
+      result.push({key:collection[i],count:count});
+      count = 1;
+      n++;
+    }
+  }
+  return result;
 }
+
 
 module.exports = count_same_elements;
