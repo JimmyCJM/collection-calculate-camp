b8260c41917551caefec5fd6c77ef3a1c9df7842
JimmyCJM <chijieming@sina.com>
2017-08-08 16:55:49 +0800

    Update amount_even.js

--- practices/superposition_operation/interval_operation/amount_even.js
+++ practices/superposition_operation/interval_operation/amount_even.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function amount_even(collection) {
-
-  //在这里写入代码
+  var a = collection.filter(value => {return value%2 === 0});
+  return a.reduce((p,value) => {
+    return p+= value},0)
 }
 
 module.exports = amount_even;
