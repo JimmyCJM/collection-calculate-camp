2bc1f223ef6b89829a9829bf4c36b2cb541e4123
JimmyCJM <chijieming@sina.com>
2017-08-08 08:51:29 +0800

    Update get_integer_interval.js

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,7 +1,21 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
+  var result = new Array();
+  if(number_a < number_b){    //判断是否自增
+    for(var i=0;i<number_b-number_a+1;i++){
+      result.push(number_a+i);
+    }
+  }
+  if (number_a > number_b){   //判断是否自减
+    for(var i=0;i<number_a-number_b+1;i++){
+      result.push(number_a-i);
+    }
+  }
+  if (number_a == number_b){
+    result.push(number_b);
+  }
+  return result;
 }
 
 module.exports = get_integer_interval;
