7cb5b059cd8cf061b5d5e2d29dabbf12b89419dc
JimmyCJM <chijieming@sina.com>
2017-08-08 09:05:38 +0800

    Update get_union.js

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
+  var result = collection_a;
+  collection_b.forEach(value =>{
+    if(!collection_a.includes(value))  result.push(value);
+  })
+  return result;
 }
 
 module.exports = get_union;
