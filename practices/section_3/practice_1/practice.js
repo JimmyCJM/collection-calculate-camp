b1d4186348fc03aa0f9177d82c928aff72e85d16
JimmyCJM <chijieming@sina.com>
2017-08-08 10:38:17 +0800

    Update practice.js

--- practices/section_3/practice_1/practice.js
+++ practices/section_3/practice_1/practice.js
@@ -1,5 +1,10 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  var result = new Array();
+  collection_a.forEach(value =>{
+    if (object_b.value.includes(value.key)) result.push({key:value.key, count:value.count-1});
+    else result.push({key:value.key, count:value.count});
+  });
+  return result;
 }
 
 module.exports = create_updated_collection;
