34692c70c492c6750c1a7adcd1afc06ea93bf720
JimmyCJM <chijieming@sina.com>
2017-08-08 10:24:45 +0800

    Update find_first_even.js

--- practices/reduce/find_first_even.js
+++ practices/reduce/find_first_even.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function find_first_even(collection) {
-  //在这里写入代码
+  var result = collection.filter(value => {
+    if (value%2 === 0) return 1;
+    else return 0;
+  });
+  return result[0];
 }
 
 module.exports = find_first_even;
