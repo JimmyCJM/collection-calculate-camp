224893d2372386a54a1c508b8056c9683dabbc5e
JimmyCJM <chijieming@sina.com>
2017-08-08 08:58:06 +0800

    Update get_letter_interval.js

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,7 +1,22 @@
 'use strict';
 
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+  var result = new Array();
+  if (number_a < number_b){   //判断是否自增
+    for(var i=0;i<number_b-number_a+1;i++){
+      result.push(String.fromCharCode(number_a+i+96));
+    }
+  }
+  if (number_a > number_b){   //判断是否自减
+    for (var i=0;i<number_a-number_b+1;i++){
+      result.push(String.fromCharCode(number_a-i+96));
+    }
+  }
+  if (number_a == number_b){
+    result.push(String.fromCharCode(number_a+96));
+  }
+  return result;
 }
+
 
 module.exports = get_letter_interval;
