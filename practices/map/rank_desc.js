fab95d11041d64f95818de56d2b536b34898a91c
JimmyCJM <chijieming@sina.com>
2017-08-08 10:11:15 +0800

    Update rank_desc.js

--- practices/map/rank_desc.js
+++ practices/map/rank_desc.js
@@ -1,6 +1,15 @@
 'use strict';
 var rank_desc = function(collection){
-  return [2,3,4,5,6];
+  return collection.sort(CompareForSort);
 };
+function CompareForSort(first, second)
+{
+  if (first == second)
+    return 0;
+  if (first < second)
+    return -1;
+  else
+    return 1;
+}
 
 module.exports = rank_desc;
