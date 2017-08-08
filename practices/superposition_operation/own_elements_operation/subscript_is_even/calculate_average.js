b10508c39f7b364accb8012b16f221adc9a28fca
JimmyCJM <chijieming@sina.com>
2017-08-08 16:59:19 +0800

    Update calculate_average.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,13 @@
 'use strict';
 var calculate_average = function(collection){
-
+  var result = 0;
+  collection = collection.filter((value,index) =>{
+    if (index%2 === 1){
+      result += value;
+      return 1;
+    }
+    else return 0;
+  })
+  return result/collection.length;
 };
 module.exports = calculate_average;
