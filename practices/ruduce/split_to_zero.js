444433fc7bedac737b2af96a29b74c5cc8a6103d
JimmyCJM <chijieming@sina.com>
2017-08-08 10:26:02 +0800

    Update split_to_zero.js

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -1,7 +1,14 @@
 'use strict';
 
-function spilt_to_zero(number, interval) {
-  //在这里写入代码
+function split_to_zero(number, interval) {
+  var result = new Array();
+  result.push(number);
+  do {
+    number = parseFloat((number - interval).toFixed(1));
+    result.push(number);
+  }
+  while(number > 0)
+  return result;
 }
 
 module.exports = spilt_to_zero;
