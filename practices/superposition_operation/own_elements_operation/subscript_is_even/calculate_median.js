406f03e50f4cdddbe62aa78599caf8d26dfe83b5
JimmyCJM <chijieming@sina.com>
2017-08-08 17:00:05 +0800

    Update calculate_median.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,12 @@
 'use strict';
 var calculate_median = function(collection){
-
+  var result = 0;
+  collection = collection.filter((value,index) =>{
+    if(index%2 === 1) return 1;
+    else return 0;
+  })
+  if (collection.length%2 === 1)  result = collection[parseInt(collection.length/2)];
+  else result = (collection[Math.ceil(collection.length/2-1)]+collection[Math.ceil(collection.length/2)])/2;
+  return result;
 };
 module.exports = calculate_median;
