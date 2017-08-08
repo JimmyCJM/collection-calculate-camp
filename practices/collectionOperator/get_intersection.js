af8a671046852be223b1931e8c41ab4b0efe1923
JimmyCJM <chijieming@sina.com>
2017-08-08 08:56:42 +0800

    Update get_intersection.js

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  //在这里写入代码
+  var result = new Array();
+  collection_b.forEach(value =>{
+    if(collection_a.includes(value))
+      result.push(value);
+  })
+  return result;
 }
-
 module.exports = get_intersection;
