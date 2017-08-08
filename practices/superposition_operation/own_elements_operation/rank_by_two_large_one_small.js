238e07981fdbfa152a02255cd6529ac4519ff627
JimmyCJM <chijieming@sina.com>
2017-08-08 17:03:21 +0800

    Update rank_by_two_large_one_small.js

--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,17 @@
 'use strict';
-function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+function rank_by_two_large_one_small(collection) {
+  collection = collection.sort(compare);
+  for (let i = 0; i < collection.length; i++) {
+    if (parseInt(i % 3) === 0 && i + 2 < collection.length) {
+      collection[i] = collection[i] ^ collection[i + 2];
+      collection[i + 2] = collection[i] ^ collection[i + 2];
+      collection[i] = collection[i] ^ collection[i + 2];
+    }
+  }
+}
+function compare(first,second) {
+  if(first === second) return 0;
+  if(first > second) return 1;
+  if(first < second)  return-1;
 }
 module.exports = rank_by_two_large_one_small;
