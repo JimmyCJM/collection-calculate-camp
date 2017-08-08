f24b9155614922bebab82a6628fb95877f363a15
JimmyCJM <chijieming@sina.com>
2017-08-08 16:54:16 +0800

    Update practice_2.js

--- practices/superposition_operation/collection_operation/practice_2.js
+++ practices/superposition_operation/collection_operation/practice_2.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
-  //在这里写入代码
+  var a = [];
+  collection.filter(value => {return value%2 === 1}).forEach(value => {
+    return  a.push(value*3+2)});
+  return a;
 }
 
 module.exports = hybrid_operation_to_uneven;
