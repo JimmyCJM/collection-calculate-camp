e1b70eab8ed52ac4d384812c8bf42fba341be17a
JimmyCJM <chijieming@sina.com>
2017-08-08 16:53:15 +0800

    Update letter_mapping_2.js

--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function average_to_letter(collection) {
-
-  //在这里写入代码
+  return String.fromCharCode(Math.ceil(collection.reduce((p,value) => {
+    return p += value;
+  },0)/collection.length)+96)
 }
 
 module.exports = average_to_letter;
