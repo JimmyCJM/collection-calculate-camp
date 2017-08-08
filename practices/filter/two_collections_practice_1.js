3cb576ffa17bf2c4356f15dcc2b5279b9381819c
JimmyCJM <chijieming@sina.com>
2017-08-08 09:46:16 +0800

    Update two_collections_practice_1.js

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function choose_common_elements(collection_a, collection_b) {
-
-  //在这里写入代码
+  return collection_a.filter(value => {
+    if(collection_b.includes(value)) return 1;
+    else return 0;
+  });
 }
 
 module.exports = choose_common_elements;
